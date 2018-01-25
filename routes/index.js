e1488be7c2819d9ca32693344d1b55ed64bc53f4
parantapmathur <30618598+parantapmathur@users.noreply.github.com>
2018-01-25 12:20:07 +0400

    adding titile

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  res.render('index', { title1: 'Mr. Parantap Mathur' });
+  res.render('index', { title: 'Express',  title1: 'Parantap Mathur' });
 });
 
 module.exports = router;
