687b277b6454f031774c83c5b67ff6b07b4d350d
parantapmathur <30618598+parantapmathur@users.noreply.github.com>
2018-01-25 12:08:18 +0400

    1st title addition

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.render('index', { title1: 'Mr. Parantap Mathur' });
 });
 
 module.exports = router;
