86ad28de9685ec10ef22074286c689ac1a49d235
parantap mathur <s732618@emirates.com>
2018-02-28 16:13:12 +0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',  title1: 'Parantap Mathur' });
-});
+});*/
 
 module.exports = router;
+
+router.get('/', function(req, res, next) {
+  console.log('Accessing the get section ...')
+  res.send('respond w ith a resource');
+  })
